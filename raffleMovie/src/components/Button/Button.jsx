606a3e37dd0bc2b71d2131d9d695062e502b5f5c
miguelbrazz <msottom2@gmail.com>
2023-05-15 17:50:15 -0300

    release beta

--- raffleMovie/src/components/Button/Button.jsx
+++
@@ -1,8 +0,0 @@
-import './Button.css'
-
-const Button = ({ children, className = '', component: Component = 'a', ...restProps }) => {
-    return <Component className={`button ${className}`} {...restProps}>{children}</Component>
-
-}
- 
-export default Button;
--- raffleMovie/src/components/Input/Input.jsx
+++
@@ -1,7 +0,0 @@
-import './Input.css'
-
-const Input = ({ type, className, name, placeholder, ...restProps }) => {
-    return <input type={type} name={name} placeholder={placeholder} {...restProps} className={className}/>
-}
- 
-export default Input;
